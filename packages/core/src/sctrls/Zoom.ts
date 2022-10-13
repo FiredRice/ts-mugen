0fd2db8fa1681c0301151d2db6c21883644d1230
wenxin <xin.wen@yoyi.com.cn>
2022-10-13 17:10:42 +0800

    fix：zoom参数规范化

--- packages/core/src/sctrls/Zoom.ts
+++ packages/core/src/sctrls/Zoom.ts
@@ -3,8 +3,18 @@ import { AttrValue, BaseSctrls } from '../types';
 import { objectToString, triggersToString, versionCheck } from '../utils';
 
 interface ZoomParams extends BaseSctrls {
-    scale?: AttrValue;
+    /**
+     * 缩放大小（必须大于 1）
+     */
+    scale?: [AttrValue, AttrValue];
+    /**
+     * 缩放位置（离屏幕正中央的距离）
+     */
     pos?: [AttrValue, AttrValue];
+    /**
+     * 缩放速率/延迟
+     */
+    lag?: AttrValue;
     facing?: AttrValue;
 }
 
